98c3e1bfa25c0c29bbdf142747b6bc9f00fe6098
TREY <hugh.a.lloyd@gmail.com>
2020-02-12 15:07:02 -0600

    still needs styling

--- githubusercard/src/components/followerCard.js
+++ githubusercard/src/components/followerCard.js
@@ -8,14 +8,17 @@ padding:3%;
 `
 
 const FollowerCard = props => {
+    console.log(props)
     return (
         <FollowerDiv className='followers-div'>
             <h1>{props.followers.login}</h1>
             <img src={props.followers.avatar_url}/>
+            <div className='p-div'>
             <p>{props.followers.name}</p>
             <p>Current Location: {props.followers.location}</p>
             <p>Bio: {props.followers.bio}</p>
             <p>{props.followers.email}</p>
+            </div>
         </FollowerDiv>
     )
 }
--- githubusercard/src/components/user.js
+++ githubusercard/src/components/user.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './user.css';
 
 
 
@@ -8,12 +9,14 @@ const User = props => {
         <div className='user-card'>
             <h1>{props.user.login}</h1>
             <img src={props.user.avatar_url}/>
+            <div className='p-div'>
             <p>{props.user.name}</p>
             <p>Current Location: {props.user.location}</p>
             <p>Bio: {props.user.bio}</p>
             <p>{props.user.email}</p>
             <p>Followers: {props.user.followers}</p>
             <p>Following: {props.user.following}</p>
+            </div>
         </div>
     )
 }
